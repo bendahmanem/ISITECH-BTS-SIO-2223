5638853218656781d312be7490ee89475b0329a6
Mounir BENDAHMANE <mounir.bendahmane@ecole-isitech.fr>
2023-03-27 11:05:48 +0200

    JS basics

---
+++ index.js
@@ -0,0 +1,11 @@
+function soustraire(a, b) {
+    return a - b;
+}
+
+const soustraire = (a, b) => {
+    return a - b;
+}
+
+const soustraire = (a, b) => a - b;
+
+const afficheConslole = chaine => console.log(chaine);
